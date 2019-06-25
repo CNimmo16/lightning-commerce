811f2985800e4840383211a6c088a8836875f6eb
EC2 Default User <ec2-user@ip-172-31-17-23.eu-west-1.compute.internal>
2019-06-25 18:45:05 +0000

    add debugging for webhooks

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -2,6 +2,7 @@ const stripe = require('stripe')(process.env.STRIPESECRET);
 
 module.exports = ({ router }) => {
     router.post("/webhook/stripe", async (ctx, next) => {
+        console.log(ctx.headers)
         const sig = ctx.headers['stripe-signature'];
         const body = ctx.request.fields
         
