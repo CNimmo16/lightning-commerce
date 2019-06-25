9f704d272ae5bca72325748a70a7917044174490
EC2 Default User <ec2-user@ip-172-31-17-23.eu-west-1.compute.internal>
2019-06-25 19:44:48 +0000

    temporarily remove signature from webhooks due to implementation issues

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -2,32 +2,30 @@ const stripe = require('stripe')(process.env.STRIPESECRET);
 
 module.exports = ({ router }) => {
     router.post("/webhook/stripe", async (ctx, next) => {
-        const sig = ctx.headers['stripe-signature'];
-        const body = ctx.request.fields
         
-        console.log("signature is " + sig)
-        console.log("body is")
-        console.log(body)
-        
-        let event = null;
-        const endpointSecret = "whsec_Jlf0Hkl7vAUiWcgz3jJDetFONygod6OY";
+        // const sig = ctx.headers['stripe-signature'];
+        // const body = ctx.request.fields
 
-        try {
-            event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
-        }
-        catch (err) {
-            // invalid signature
-            ctx.status = 400;
-            return await next();
-        }
+        // let event = null;
+        // const endpointSecret = "whsec_Jlf0Hkl7vAUiWcgz3jJDetFONygod6OY";
+
+        // try {
+        //     event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
+        // }
+        // catch (err) {
+        //     // invalid signature
+        //     ctx.status = 400;
+        //     return await next();
+        // }
+        // const intent = event.data.object
+        
+        const intent = ctx.request.fields
         
         if(event.type === "payment_intent.succeeded") {
-            const intent = event.data.object;
             console.log(intent)
             // intent.id 
             ctx.body = "successfully captured payment and sent order for fulfillment"
         } else if(event.type === "payment_intent.payment_failed") {
-            const intent = event.data.object;
             const message = intent.last_payment_error && intent.last_payment_error.message;
             console.log('Failed:', intent.id, message);
             ctx.body = "successfully acknowledged failed payment, contacting customer by email"
